a4d8522a7e87ca35a3557f2a1e45c51c72c00be0
Nick Lan <aa0975766121@gmail.com>
2023-07-24 18:51:49 +0800

    todo list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
 import { Component } from '@angular/core';
 
+interface List {
+  title: string;
+  checked: boolean;
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'todolist';
+  todos: List[] = [
+    { title: 'Hit the gym', checked: false },
+    { title: 'Pay bills', checked: true },
+    { title: 'Meet George', checked: false },
+    { title: 'Buy eggs', checked: false },
+    { title: 'Read a book', checked: false },
+    { title: 'Organize office', checked: false },
+  ];
+
+  newTodo: string = '';
+
+  addTodo() {
+    if (this.newTodo) {
+      this.todos.push({ title: this.newTodo, checked: false });
+      this.newTodo = '';
+    } else {
+      alert('You must write something!');
+    }
+  }
+  deleteTodo(index: number) {
+    this.todos.splice(index, 1);
+  }
 }
